9354a7b41c0264aa4df91121076bcde371f8c1c4
AndLuna <contato.andreluna@gmail.com>
2022-12-20 17:03:11 -0300

    final

--- listinha2.js
+++ listinha2.js
@@ -1,33 +1,31 @@
-
-
-// 8- Um novo modelo de carro da Tesal, super econômico foi lançado.
-// Ele faz 26km com 1 litro de combustível.
-// Cada litro de combustível custa R$ 6,29.
-
-// Faça um programa que pergunte ao usuário quanto de dinheiro ele tem e em seguida diga quantos litros de combustível ele pode comprar e quantos kilometros o carro consegue andar com este tanto de combustível.
-
-// function autonomie(){
-//     var dinheiro = parseFloat(document.getElementById("dinheiro").value);
-//     let litroValor = 6.29;
-//     let km = 26;
-//     let autonomia =  Math.round(dinheiro / litroValor) * km;
- 
-//     if(autonomia > 0) {
-//         alert("Sua autonomia e de "+autonomia+'Km');
-//     }
-     
-//     else{
-//         alert("Coloque algum valor!")
-//     } 
-//    }
-
 function autonomie(){
     var dinheiro = parseFloat(document.getElementById("dinheiro").value);
     let litroValor = 6.29;
     let km = 26;
     let autonomia =  (dinheiro / litroValor) * km;
  
-   document.getElementById('autom').value =  autonomia.toFixed(2)
-   }
-  
+//    document.getElementById('autom').value =  autonomia.toFixed(2)
+   document.getElementById("x").innerHTML = `Com R$ ${dinheiro},00 Reais voce tera uma autonomia de ${autonomia.toFixed(0)}km. 
+   Total de 26km por litro.`
+
+}
+
+////////////  usando a function para esconder a div ate que o btn seja clickado
+
+
+
+var btn = document.querySelector("#btn");
+btn.addEventListener("click", function() {
+    var div = document.querySelector("#totaut");
+    
+  if(div.style.display === "block") {
+        div.style.display = "none";
+    } else {
+      div.style.display = "block";
+  }
+    
+});
+
+/////////// tentando pegar o valor de dinheiro 
+
 
--- marcos/app.js
+++ marcos/app.js
@@ -1,25 +1,11 @@
-function hello () {
-    var nome;
-    nome = prompt("Qual é o seu nome ?")
-    alert("hello"+" "+nome+" "+"é um prazer telo conosco, vamos começar a inserção dos dados ?");
-}
-
-var etanol, gasolina;
-function Calcular(){
-    etanol = parseFloat (frmFlex.txtEtanol.value.replace(",","."));
-    gasolina = parseFloat (frmFlex.txtGasolina.value.replace(",","."));
-   
-    if (etanol< 0.7 * gasolina ){
-document.getElementById("status").src="etanol.png";
-    }
-    else{
-        document.getElementById("status").src="gasolina.png";
-
-    }
-
-}
-
-function resetar () {
-    document.getElementById("status").src="neutro.png";
-
-}
+var btn = document.querySelector("#btn");
+btn.addEventListener("click", function() {
+    var div = document.querySelector("#container");
+    
+  if(div.style.display === "block") {
+        div.style.display = "none";
+    } else {
+      div.style.display = "block";
+  }
+    
+});
